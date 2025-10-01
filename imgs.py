502bb2e19ba042fcb2147dfea727bae1d6cae557
acouch <acinternets@gmail.com>
2025-10-01 15:52:59 -0400

    Update for new data

--- imgs.py
+++ imgs.py
@@ -20,7 +20,7 @@ rows = []
 
 for i,row in enumerate(csv_data):
     if (row[6] and i > 0):
-        url = row[7]
+        url = row[6]
         data = requests.get(url)
         id = row[0]
         file_type = data.headers['content-type']
@@ -36,10 +36,9 @@ for i,row in enumerate(csv_data):
         elif (file_type == 'image/gif'):
             ext = 'gif'
         if (ext == ''):
-            print(url)
             print(file_type, data.status_code, id)
         else:
-            path = 'imgs/logos/' + id + '.' + ext
+            path = 'assets/imgs/logos/' + id + '.' + ext
             f = open(path,'wb')
             f.write(data.content)
             f.close()
